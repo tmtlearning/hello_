160af5a2f96b70c009f9845b6a8034e8d40c672d
tmtlearning <gb0456@c.vztc.com>
2012-11-23 11:45:34 +0400

    Update p.py

--- p.py
+++ p.py
@@ -1,6 +1,9 @@
-def fact(n):
-     if n == 0:
-          return 1
-     return fact(n-1) * n
+def fact(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
 
 print(fact(100))
